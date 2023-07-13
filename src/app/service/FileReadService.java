d3737a2c578553b310dea5fce41b42541bf60375
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-07-13 22:17:33 +0300

    Create class FileReadService

---
+++ src/app/service/FileReadService.java
@@ -0,0 +1,19 @@
+package app.service;
+
+import app.utils.Constants;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileReadService {
+    public String readFromFile(String fileName) {
+        Path path = Paths.get(Constants.BASE_PATH + fileName);
+        try {
+            return Files.readString(path);
+        } catch (IOException ex) {
+            return "Something wrong " + ex.getMessage();
+        }
+    }
+}

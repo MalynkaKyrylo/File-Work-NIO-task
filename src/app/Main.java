4a7b6463bbe52929de45a465a1e6eab036a878ae
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-07-14 00:59:24 +0300

    Add NumberFormatException to getChoise method

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,7 @@ package app;
 
 import app.service.FileReadService;
 import app.service.FileWriteService;
+
 import java.util.Scanner;
 
 public class Main {
@@ -9,13 +10,19 @@ public class Main {
     public static void main(String[] args) {
         handleChoice(getChoice());
     }
+
     private static int getChoice() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Select the file processing option:\n" +
-                "1) Create and write in file\n" +
-                "2) Read file\n" +
-                "0) Close the app");
-        return scanner.nextInt();
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Select the file processing option:\n" +
+                    "1) Create and write in file\n" +
+                    "2) Read file\n" +
+                    "0) Close the app");
+            return Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Not a number input!");
+            return getChoice();
+        }
     }
 
     private static void handleChoice(int choice) {
@@ -30,29 +37,27 @@ public class Main {
                 System.out.println("App closed");
                 System.exit(0);
             default:
-            System.out.println("Wrong value" + choice);
+                System.out.println("Wrong value " + choice);
+                handleChoice(choice);
         }
     }
 
     private static String getWriteData() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your content: ");
-        String content = scanner.nextLine();
-        return content;
+        return scanner.nextLine();
     }
 
     private static String getFileName() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter file name: ");
-        String fileName = scanner.nextLine();
-        return fileName;
+        return scanner.nextLine();
     }
 
     private static String getReadData() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter file name to show it's content: ");
-        String fileName = scanner.nextLine();
-        return fileName;
+        return scanner.nextLine();
     }
 
     private static String readFile(String file) {
